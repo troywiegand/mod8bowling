703e0b16dd8e65a61ba249f069fe96f0a7c0ba2a
Troy Wiegand <troy@troywiegand.com>
2018-07-27 17:40:42 -0400

    Add basic about page

---
+++ src/About.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {NavLink} from 'react-router-dom'
+
+
+class About extends Component {
+
+ 
+
+  render() {
+    return (
+      <div className="About">
+    <center><h1>mod 8 bowling</h1>
+    <p> 
+        mod 8 Bowling is a variant on regular bowling. 
+    </p>
+        
+        <br />
+        
+    </center>
+      </div>
+      
+    );
+  }
+
+  
+}
+
+
+
+
+
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route , Switch, Redirect} from 'react-router-dom'
+
+import About from './About'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+constructor()
+{
+super()
 }
 
-export default App;
+render(){
+    return(
+        <Switch>
+            
+            <Route
+            path='/about'
+            component={About}
+            />
+            <Route
+            render={()=>{
+                return(
+                    <Redirect to='/about'/>
+                )
+            }}
+            />
+        </Switch>
+    )
+}
+
+}
+
+export default App
