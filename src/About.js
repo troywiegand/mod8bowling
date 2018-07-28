5503129a3a2499ae9b56a7d8e755fb794bd91985
Troy Wiegand <troy@troywiegand.com>
2018-07-27 20:04:00 -0400

    Edit the text on the About page

--- src/About.js
+++ src/About.js
@@ -11,11 +11,11 @@ class About extends Component {
       <div className="About">
     <center><h1>mod 8 bowling</h1>
     <p> 
-        mod 8 Bowling is a variant on regular bowling. 
+        mod 8 Bowling is a variant on inferior bowling. 
     </p>
         
         <br />
-        <p> If you would like to see your score:
+        <p> If you would like to calculate your score:
            <NavLink to={"/score"}>  
              Click Here 
             </NavLink>
